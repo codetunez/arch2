006d7287f64c73aef849530973f023ca5b32ba45
Samrit Sangal <samritsangal@microsoft.com>
2022-10-13 16:52:43 -0400

    Bootstrap v5 renderer

--- library/index.js
+++ library/index.js
@@ -97,9 +97,24 @@ module.exports.engines = {
     "bootstrap5": (markup) => {
         let $ = cheerio.load(markup, { xmlMode: true }, false);
 
+        // TODO: what if multiple navs in header 
+        $(`header nav`).addClass("navbar navbar-expand-lg bg-light")
+        $(`header nav ul`)
+            .addClass("navbar-nav navbar-right")
+            .wrap($(`<div class="collapse navbar-collapse justify-content-end" id="navbarNav"></div>`))
+        $(`header nav a[data-pp-element="brand"]`)
+            .addClass("navbar-brand")
+        $(`<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>`)
+            .insertAfter($(`header nav a[data-pp-element="brand"]`))
+        $(`header nav`).wrapInner($(`<div class="container-fluid"></div>`))
+        $(`ul.navbar-nav li`).addClass("nav-item")
+        $(`ul.navbar-nav li a`).addClass("nav-link")
+
         $(`div[data-pp-element="grid"]`).addClass("container")
         $(`div[data-pp-element="row"]`).addClass("row")
-        $(`div[data-pp-element="column"]`).addClass("column")
+        $(`div[data-pp-element="column"]`).addClass("col")
         $("button").addClass("btn btn-primary")
         // TODO: form groups, help blocks ?
         $("input").addClass("form-control")
@@ -226,8 +241,8 @@ module.exports.templates = {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
         ${styles || ""}
     </head>
     <body>
